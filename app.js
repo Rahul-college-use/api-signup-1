6f053b74061e2cd78a684a2dd70528538870f79b
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-02 22:27:58 +0530

    model

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
